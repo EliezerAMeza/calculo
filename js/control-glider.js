a28ae05e1e5d0d2450feeb1f952f2611ee20f332
EliezerAMeza <74123454+EliezerAMeza@users.noreply.github.com>
2020-11-08 18:12:13 -0400

    Add files via upload

--- js/control-glider.js
+++ js/control-glider.js
@@ -1,45 +1,12 @@
 window.addEventListener("load", ()=>{
-    new Glider(document.querySelector(".carousel__lista-1"),{
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        draggable: true,
-        dots: ".dots-1",
-        arrows: {
-            prev: ".arrow-p-1",
-            next: ".arrow-n-1"
-        }
-    });
-
-    new Glider(document.querySelector(".carousel__lista-2"),{
+    new Glider(document.querySelector(".carousel__lista"),{
         slidesToShow: 1,
         slidesToScroll: 1,
         draggable: true,
         dots: ".carousel__indicadores",
         arrows: {
-            prev: ".arrow-p-2",
-            next: ".arrow-n-2"
-        }
-    });
-
-    new Glider(document.querySelector(".carousel__lista-3"),{
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        draggable: true,
-        dots: ".carousel__indicadores",
-        arrows: {
-            prev: ".arrow-p-3",
-            next: ".arrow-n-3"
-        }
-    });
-
-    new Glider(document.querySelector(".carousel__lista-4"),{
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        draggable: true,
-        dots: ".carousel__indicadores",
-        arrows: {
-            prev: ".arrow-p-4",
-            next: ".arrow-n-4"
+            prev: ".carousel__anterior",
+            next: ".carousel__siguiente"
         }
     });
 })
